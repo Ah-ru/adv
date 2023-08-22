2de45086bdd0daf89f551343b838c3abca389e21
Alexander Haisov <ah.coder@gmail.com>
2023-08-22 22:01:13 +0300

    HW-7

--- advertisements/models.py
+++ advertisements/models.py
@@ -10,9 +10,8 @@ class Adv(models.Model):
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
     auction = models.BooleanField(help_text = "Select - auction or not")
-
-class Meta:
-    db_table = "advertisements"
-    
-    def __str__(self):
-        return f"<Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price})"
+    class Meta:
+        db_table = "advertisements"
+        
+        def __str__(self):
+            return f"<Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price})"
